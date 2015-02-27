70dddf1d735a309a5264bc5d1082041811a3ec17
benhowell <bjhowell@gmail.com>
2015-02-27 22:51:17 +1100

    moved to lambda stream stuff, moved to typesafe config

--- src/java/net/benhowell/controller/PictureWithTextAndTextBoxController.java
+++ src/java/net/benhowell/controller/PictureWithTextAndTextBoxController.java
@@ -24,12 +24,13 @@
 
 package net.benhowell.controller;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigException;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -38,9 +39,9 @@ import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import net.benhowell.core.Display;
+import net.benhowell.core.Store;
 
 import java.net.URL;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 /**
@@ -52,6 +53,8 @@ import java.util.ResourceBundle;
  */
 public class PictureWithTextAndTextBoxController extends ScreenController implements Initializable {
 
+  private Store store;
+  private Config config;
   private ImageView imageView = null;
   private Label label = null;
   private TextField textField = null;
@@ -59,9 +62,10 @@ public class PictureWithTextAndTextBoxController extends ScreenController implem
   private Label validationLabel = null;
   @FXML private HBox hBox;
 
-  public PictureWithTextAndTextBoxController(ControllerLoader loader, String resource, Display display){
+  public PictureWithTextAndTextBoxController(ControllerLoader loader, String resource, Display display, Store store){
     super(loader, resource, display);
 
+    this.store = store;
 
     borderPane = new BorderPane();
     borderPane.prefHeight(200.0);
@@ -109,9 +113,6 @@ public class PictureWithTextAndTextBoxController extends ScreenController implem
     GridPane.setRowIndex(textField, 3);
     gridPane.getChildren().add(textField);
 
-
-    /*<Label id="label" fx:id="validationLabel" prefHeight="30.0" prefWidth="568.0000999999975" text="Error: Please enter a name containing alphabetical characters only!" visible="false" />
-      */
     validationLabel = new Label();
     validationLabel.setPrefHeight(30.0);
     validationLabel.setPrefWidth(568.0000999999975);
@@ -119,10 +120,7 @@ public class PictureWithTextAndTextBoxController extends ScreenController implem
     validationLabel.setVisible(false);
     hBox.getChildren().add(0,validationLabel);
 
-
-
     nextButton.setDisable(true);
-
 
     textField.textProperty().addListener((observable, oldValue, newValue) -> {
       if(newValue.matches("^[a-zA-Z\\s]+$"))
@@ -132,7 +130,6 @@ public class PictureWithTextAndTextBoxController extends ScreenController implem
       else
         invalidInput();
     });
-
 
     ColumnConstraints columnConstraints = new ColumnConstraints();
     columnConstraints.setHgrow(Priority.NEVER);
@@ -159,18 +156,27 @@ public class PictureWithTextAndTextBoxController extends ScreenController implem
     r3.setValignment(VPos.TOP);
     r3.setVgrow(Priority.NEVER);
 
-
     r4.setMinHeight(10.0);
     r4.setPrefHeight(60.0);
     r4.setValignment(VPos.CENTER);
     r4.setVgrow(Priority.ALWAYS);
-
 
     r5.setMinHeight(20.0);
     r5.setPrefHeight(35.0);
     r5.setVgrow(Priority.NEVER);
 
     gridPane.getRowConstraints().addAll(r1, r2, r3, r4, r5);
+
+
+    this.prevButton.setOnAction(e -> triggerPrevButtonEvent());
+
+    this.nextButton.setOnAction(e -> {
+      if(!config.hasPath("example"))
+        store.addTrial(config, getResult());
+      triggerNextButtonEvent();
+    });
+
+
 
 
   }
@@ -181,28 +187,42 @@ public class PictureWithTextAndTextBoxController extends ScreenController implem
   }
 
 
-  public void update(Map<String, String> trial) {
-    System.out.println("trial: " + trial);
-
-    String txt = trial.get("text").replace("${name}", trial.get("name"));
-    Image img = new Image(trial.get("imagePath") + trial.get("imageName"));
+  public void update(Config config) {
+    this.config = config;
+    String txt = config.getString("text").replace("${name}", config.getString("name"));
+    Image img = new Image(this.getClass().getResource("/img/").toString() + config.getString("image"));
 
     super.update(() -> {
       textField.clear();
       label.setText(txt);
       imageView.setImage(img);
-    });
 
-    textField.requestFocus();
+      if(config.hasPath("example")) { // config item is an example
+        textField.setText(config.getString("example"));
+        textField.setEditable(false);
+        nextButton.requestFocus();
+      }
+      else { // config item is a trial
+        Config trial = store.getTrial(config.getString("id"));
+        if(trial != null)
+          textField.setText(trial.getString("result"));
+        textField.setEditable(true);
+        textField.requestFocus();
+      }
+
+    });
   }
+
 
   public void initialize(URL url, ResourceBundle rb) {
     System.out.println(this.getClass().getSimpleName() + ".initialise");
   }
 
+
   private void reset(){
     textField.requestFocus();
   }
+
 
   private void emptyInput(){
     nextButton.setDisable(true);
@@ -211,6 +231,7 @@ public class PictureWithTextAndTextBoxController extends ScreenController implem
     textField.requestFocus();
   }
 
+
   private void invalidInput(){
     nextButton.setDisable(true);
     switchStyle(validationLabel, "validation_valid", "validation_error");
@@ -218,25 +239,12 @@ public class PictureWithTextAndTextBoxController extends ScreenController implem
     textField.requestFocus();
   }
 
+
   private void validInput(){
     nextButton.setDisable(false);
     switchStyle(validationLabel, "validation_error", "validation_valid");
     switchStyle(textField, "validation_error", "validation_valid");
     textField.requestFocus();
   }
-
-  private void switchStyle(Node node, String oldStyle, String newStyle){
-    removeStyle(node, oldStyle);
-    addStyle(node, newStyle);
-  }
-
-  private void addStyle(Node node, String style) {
-    node.getStyleClass().add(style);
-  }
-
-  private void removeStyle(Node node, String style) {
-    node.getStyleClass().remove(style);
-  }
-
 }
 
