2a75b19ff61de5611bf35fafd65e54eada9577c9
benhowell <bjhowell@gmail.com>
2015-03-04 21:45:18 +1100

    added xlsx file generation functionality

--- src/java/net/benhowell/core/Store.java
+++ src/java/net/benhowell/core/Store.java
@@ -25,11 +25,15 @@
 package net.benhowell.core;
 
 import com.typesafe.config.Config;
-import com.typesafe.config.ConfigValue;
 import com.typesafe.config.ConfigValueFactory;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,7 +43,9 @@ import java.util.Map;
  */
 public class Store {
 
-  HashMap<String, Config> results = new HashMap<>();
+  HashMap<String, Config> trials = new HashMap<>();
+  Config details;
+  Map<String, String> languages = new HashMap<>();
 
   public void addTrial(Config trial, String result){
     Config newConfig = trial.withValue("result",  ConfigValueFactory.fromAnyRef(result));
@@ -56,43 +62,141 @@ public class Store {
     System.out.println(" presented text: " + txt);
     System.out.println(" result: " + result);
     System.out.println("-------------------------\n");
-    results.put(newConfig.getString("id"), newConfig);
+    trials.put(newConfig.getString("id"), newConfig);
   }
 
   public Config getTrial(String id){
-    return results.remove(id);
+    return trials.remove(id);
   }
 
-  public void addDetail(Map<String, String> result){
-    System.out.println("\nStoring result");
+  public void addDetails(Map<String, String> detail){
+    System.out.println("\nStoring details");
     System.out.println("-------------------------");
-    System.out.println(" sex: " + result.get("sex"));
-    System.out.println(" age: " + result.get("age"));
-    System.out.println(" yearsInAustralia: " + result.get("yearsInAustralia"));
-    System.out.println(" monthsInAustralia: " + result.get("monthsInAustralia"));
-    System.out.println(" firstLanguage: " + result.get("firstLanguage"));
+    System.out.println(" sex: " + detail.get("sex"));
+    System.out.println(" age: " + detail.get("age"));
+    System.out.println(" yearsInAustralia: " + detail.get("yearsInAustralia"));
+    System.out.println(" monthsInAustralia: " + detail.get("monthsInAustralia"));
+    System.out.println(" firstLanguage: " + detail.get("firstLanguage"));
     System.out.println("-------------------------\n");
-    results.put("detail", ConfigValueFactory.fromMap(result).toConfig());
+    details = ConfigValueFactory.fromMap(detail).toConfig();
   }
 
-  public Config getDetail(){
-    return results.remove("detail");
+  public Config getDetails(){
+    return details;
   }
 
-  public void addLanguage(Map<String, String> result){
+  public void addLanguages(Map<String, String> l){
     System.out.println("\nStoring result");
     System.out.println("-------------------------");
-    for (Map.Entry<String, String> entry : result.entrySet()) {
+    for (Map.Entry<String, String> entry : l.entrySet()) {
       System.out.println(" " + entry.getKey() + ": " + entry.getValue());
     }
     System.out.println("-------------------------\n");
-    results.put("languages", ConfigValueFactory.fromMap(result).toConfig());
+
+    languages = l;
+  }
+
+  public Map<String, String> getLanguages(){
+    return languages;
+  }
+
+  public void generateXlsx(File file){
+
+    try {
+      FileOutputStream out = new FileOutputStream(file.getAbsoluteFile());
+      SXSSFWorkbook wb = new SXSSFWorkbook(100);
+      Sheet shDetails = wb.createSheet("Details");
+      Sheet shLanguages = wb.createSheet("Languages");
+      Sheet shResults = wb.createSheet("Results");
+
+      Row row = shDetails.createRow(0);
+      Cell cell = row.createCell(0);
+      cell.setCellValue("sex");
+      cell = row.createCell(1);
+      cell.setCellValue("age");
+      cell = row.createCell(2);
+      cell.setCellValue("years in Australia");
+      cell = row.createCell(3);
+      cell.setCellValue("months in Australia");
+      cell = row.createCell(4);
+      cell.setCellValue("first language");
+
+      row = shDetails.createRow(1);
+      cell = row.createCell(0);
+      cell.setCellValue(details.getString("sex"));
+      cell = row.createCell(1);
+      cell.setCellValue(details.getString("age"));
+      cell = row.createCell(2);
+      cell.setCellValue(details.getString("yearsInAustralia"));
+      cell = row.createCell(3);
+      cell.setCellValue(details.getString("monthsInAustralia"));
+      cell = row.createCell(4);
+      cell.setCellValue(details.getString("firstLanguage"));
+
+
+      int i = 1;
+      row = shLanguages.createRow(0);
+      cell = row.createCell(0);
+      cell.setCellValue("language");
+      cell = row.createCell(1);
+      cell.setCellValue("fluency");
+      for (Map.Entry<String, String> e : languages.entrySet()) {
+        row = shLanguages.createRow(i);
+        cell = row.createCell(0);
+        cell.setCellValue(e.getKey());
+        cell = row.createCell(1);
+        cell.setCellValue(e.getValue());
+        i++;
+      }
+
+
+      i = 1;
+      row = shResults.createRow(0);
+      cell = row.createCell(0);
+      cell.setCellValue("trial_id");
+      cell = row.createCell(1);
+      cell.setCellValue("category");
+      cell = row.createCell(2);
+      cell.setCellValue("type");
+      cell = row.createCell(3);
+      cell.setCellValue("image");
+      cell = row.createCell(4);
+      cell.setCellValue("text");
+      cell = row.createCell(5);
+      cell.setCellValue("name");
+      cell = row.createCell(6);
+      cell.setCellValue("result");
+      for (Map.Entry<String, Config> e : trials.entrySet()) {
+        row = shResults.createRow(i);
+        cell = row.createCell(0);
+        cell.setCellValue(e.getValue().getString("id"));
+        cell = row.createCell(1);
+        cell.setCellValue(e.getValue().getString("category"));
+        cell = row.createCell(2);
+        cell.setCellValue(e.getValue().getString("type"));
+        cell = row.createCell(3);
+        cell.setCellValue(e.getValue().getString("image"));
+        cell = row.createCell(4);
+        cell.setCellValue(e.getValue().getString("text"));
+        cell = row.createCell(5);
+        cell.setCellValue(e.getValue().getString("name"));
+        cell = row.createCell(6);
+        cell.setCellValue(e.getValue().getString("result"));
+        i++;
+      }
+
+      wb.write(out);
+      out.close();
+
+      // dispose of temporary files backing this workbook on disk
+      wb.dispose();
+    }
+    catch (IOException e){
+      System.out.println("Exception: " + e);
+    }
 
   }
 
-  public Config getLanguage(){
-    return results.remove("languages");
-  }
 
   public String generateCSV() {
     StringBuilder sb = new StringBuilder();
@@ -102,15 +206,15 @@ public class Store {
     sb.append("sex" + sep + "age" + sep + "years in Australia" + sep + "months in Australia" + sep + "first language");
     sb.append('\n');
 
-    sb.append(results.get("detail").getString("sex"));
+    sb.append(trials.get("detail").getString("sex"));
     sb.append(sep);
-    sb.append(results.get("detail").getString("age"));
+    sb.append(trials.get("detail").getString("age"));
     sb.append(sep);
-    sb.append(results.get("detail").getString("yearsInAustralia"));
+    sb.append(trials.get("detail").getString("yearsInAustralia"));
     sb.append(sep);
-    sb.append(results.get("detail").getString("monthsInAustralia"));
+    sb.append(trials.get("detail").getString("monthsInAustralia"));
     sb.append(sep);
-    sb.append(results.get("detail").getString("firstLanguage"));
+    sb.append(trials.get("detail").getString("firstLanguage"));
     sb.append('\n');
     sb.append('\n');
 
@@ -118,7 +222,7 @@ public class Store {
 
     sb.append("language" + sep + "fluency");
     sb.append('\n');
-    results.get("languages").entrySet()
+    trials.get("languages").entrySet()
         .stream()
         .forEach(e -> {
           sb.append(e.getKey().toString());
@@ -132,7 +236,7 @@ public class Store {
     sb.append("trial_id" + sep + "category" + sep + "type" + sep + "image" + sep + "text" + sep + "name" + sep + "result");
     sb.append('\n');
 
-    results.entrySet()
+    trials.entrySet()
       .stream()
       .forEach(e -> {
         if (e.getKey().startsWith("trial")) {
